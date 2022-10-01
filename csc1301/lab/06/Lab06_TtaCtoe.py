d25b6957dafb96724d8df9259894bf0bf5794080
Motaphe <suzalregmii@gmail.com>
2022-09-30 20:37:31 -0400

    IT IS WHAT IT IS

---
+++ csc1301/lab/06/Lab06_TtaCtoe.py
@@ -0,0 +1,46 @@
+tik_board = [input(f"Row {_} (can only be 'X', 'O', or 'E'): ") for _ in range(0, 3)]
+
+to_print = []
+
+for i in range(0, 3):
+    if len(set(tik_board[i])) == 1:
+        if to_print.count(f"{tik_board[i][0]} IS GOOD IN HORIZONTAL") == 0:
+            to_print.append(f"{tik_board[i][0]} IS GOOD IN HORIZONTAL")
+
+
+if tik_board[0][0] == tik_board[1][1]:
+    if tik_board[1][1] == tik_board[2][2]:
+        if to_print.count(f"{tik_board[0][0]} IS GOOD IN DIAGONAL") == 0:
+            to_print.append(f"{tik_board[0][0]} IS GOOD IN DIAGONAL")
+
+if tik_board[0][2] == tik_board[1][1]:
+    if tik_board[1][1] == tik_board[2][0]:
+        if to_print.count(f"{tik_board[0][2]} IS GOOD IN DIAGONAL") == 0:
+            to_print.append(f"{tik_board[0][2]} IS GOOD IN DIAGONAL")
+
+for i in range(0,3):
+    if tik_board[0][i] == tik_board[1][i]:
+        if tik_board[1][i] == tik_board[2][i]:
+            if to_print.count(f"{tik_board[0][i]} IS GOOD IN VERTICAL") == 0:
+                to_print.append(f"{tik_board[0][i]} IS GOOD IN VERTICAL")
+
+
+try:
+    to_print.remove("E IS GOOD IN HORIZONTAL")
+except (ValueError):
+    pass
+try:
+    to_print.remove("E IS GOOD IN DIAGONAL")
+except (ValueError):
+    pass
+try:
+    to_print.remove("E IS GOOD IN VERTICAL")
+except (ValueError):
+    pass
+
+
+if len(to_print) != 0:
+    for i in range(len(to_print)):
+        print (to_print[i])
+else:
+    print ("THIS IS TIE")
