dd717900fe255b56c69e8a1af5777fb77868467a
Motaphe <suzalregmii@gmail.com>
2022-10-02 21:17:06 -0400

    I AM DONE

--- csc1301/hw/tophat/TheGuess_original.py
+++ csc1301/hw/tophat/TheGuess_original.py
@@ -10,19 +10,38 @@ needed after the lower and upper bounds are entered.)
 """
 
 import random
+
 smaller = int(input("Enter the smaller number: "))
 larger = int(input("Enter the larger number: "))
+what_to_guess = int(input("Enter the number to be guessed: "))
+minimum_tries = int(input("How many tries do you want to give the computer?: "))
 count = 0
-while True:
+
+while minimum_tries != 0:
     count += 1
     userNumber = int(random.randint(smaller, larger))
     check = input(f"Is is the number {userNumber} ?(<,=,>): ")
-    if check == "=":
-        print ("YaY!")
-        print (f"Only took me {count} tries to get the answer.")
-        break
+    if what_to_guess == userNumber :
+        if check == "=":
+            print ("YaY!")
+            print (f"Only took me {count} tries to get the answer.")
+            break
+        else:
+            print ("CHEATING ALERT!!")
+
+    elif what_to_guess < userNumber:    
+        if check == "<":
+            larger = userNumber - 1
+        else:
+            print ("CHEATING ALERT!!")
+
+    elif  what_to_guess > userNumber:
+        if check == ">":
+            smaller = userNumber + 1
+        else:
+            print ("CHEATING ALERT!!")
+
+    minimum_tries -= 1
     
-    if check == "<":
-        smaller = userNumber
-    if check == ">":
-        larger = userNumber
+if minimum_tries == 0:
+    print ("Dang it! I failed to guess the number in time :(")
