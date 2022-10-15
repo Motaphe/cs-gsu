0325aaa1699198ec600a5836cdde2d389c22fe00
Motaphe <suzalregmii@gmail.com>
2022-10-14 21:19:40 -0400

    lab08

--- csc1301/lab/08/loop_de_loop.py
+++ csc1301/lab/08/loop_de_loop.py
@@ -1,14 +1,40 @@
+## Prolog
+# Author: Suzal Regmi
+# Email: sregmi2@student.gsu.edu
+# Section: 036
+# Timestamp: 09:20 pm, October 14
+
+'''
+Purpose: 
+To do some particular computations
+
+Pre-Conditions (input):
+Numbers for particular computations.
+
+Post-Conditions (output):
+List of numbers depending on the particular computation.
+'''
+
+## The Design:
+
+# Define the function that makes a list starting from 0
+# to the particular number, but doesnt include the number. 
 def makelist(num):
     return [i for i in range(num)]
 
+# Define the function that does a countdown starting from the particular
+# number to 1 and then displays the launch off message.
 def rocketcountdown(num):
     lst = [i for i in range(num, 0, -1)]
     lst.append("We have lift off!")
     return lst
 
+# Define the function that takes two numbers as inputs and returns
+# the relevant list as output.
 def doubleloop(num1, num2):
     return [f"{i}:{j}" for i in range(num1) for j in range(num2)]
-    
+
+# Define the main function to text the other functions    
 def main():
     print (makelist(10))
     print (makelist(3))
@@ -19,5 +45,6 @@ def main():
     print (doubleloop(3, 3))
     print (doubleloop(3, 4))
 
+# Call the main function only if directly running this particular program.
 if __name__ == "__main__":
     main()
